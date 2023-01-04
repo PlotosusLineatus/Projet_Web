89e835f7012f87d8b4ff93f0e051d2f5e21c2d56
HERMAN <95212580+Sherman-1@users.noreply.github.com>
2023-01-04 16:35:25 +0100

    Update load_data.py

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -6,8 +6,10 @@ from Bio import SeqIO
 # Mandatory run() procedure name, runscript doest not recognize if not
 def run(): 
 
+	# Need to let user enter absolute path 
 	os.chdir("/home/sherman/Documents/M2/WEB/data")
 	
+	# Not good enough but work for now
 	cds = glob.glob("*cds*")
 	protein = glob.glob("*pep*")
 	full = glob.glob("*")
