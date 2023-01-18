de229198c8fefd14e5dd869fda939d16d689e5bf
Sherman-1 <herman.simon.lm@gmail.com>
2023-01-18 17:41:44 +0100

    Parser + loader de toutes les données d'entrée

--- genomeBact/models.py
+++ genomeBact/models.py
@@ -1,35 +1,15 @@
 from django.db import models
 from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
-import Bio
-
-
-
-
-class testClass(models.Model):
-
-    field1 = models.CharField(default= "", max_length=50)
-
-    def show(self):
-
-        return ("Field : %s" % self.field1)
 
 
 class Genome(models.Model):
 
 
-    strain = models.CharField(max_length = 50, default = "")  # Not unique because of several strains, experimental conditions etc : bact. adaptation very quick
-    chromosome = models.CharField(max_length = 30, primary_key= True, help_text = "Chromosome version name")
+    strain = models.CharField(max_length = 50, unique = True)
+    chromosome = models.CharField(max_length = 30, help_text = "Chromosome version name", default = "")
     sequence = models.TextField(default = "", help_text = "Copy FASTA sequence here")
 
-    def show(self):
-        print(self.chromosome)
-        print(self.strain)
-
-    @property
-    def length(self): 
-        return len(self.sequence)
-        
 #    def get_absolute_url(self):
 #        return reverse('model-detail-view', args=[str(self.chromosome)])
 
@@ -38,44 +18,10 @@ class Transcript(models.Model):
     ## Physical informations 
 
     # One unique ID per CDS / Protein 
-    ID = models.CharField(max_length=50, primary_key= True)
+    name = models.CharField(max_length=50, unique = True)
+    genome = models.ForeignKey(Genome, related_name = "transcript", on_delete = models.CASCADE)
     seq_cds = models.TextField(default = "")
     seq_nt = models.TextField(default = "")
-
-    chromosome = models.ForeignKey(Genome, null= False, on_delete=models.CASCADE)    
-
     start = models.IntegerField(null = True)
     stop = models.IntegerField(null = True)
-
-    ## Annotation 
-    gene = models.CharField(max_length = 10, null = True)
-    gene_biotype = models.CharField(max_length = 100, default = "protein coding")
-    description = models.CharField(max_length = 100, default = "")
-
-    # Not functionnal : can validate 
-    gene_symbol = models.CharField(max_length = 20, null = True, validators=[RegexValidator("^[A-Za-z0-9]+$", message= "OLAAAAAAAA OH")])
-
-
-    # Calculate length and store it as field as Transcript.length return actual length
-    @property
-    def length(self):
-        return self.stop - self.start
-
-    def show(self):
-            print(self.ID)
-            print(self.gene)
-            print(self.gene_symbol)
-            print(self.length)
-    
-
-class Annotation(models.Model):
-    transcript = models.ForeignKey(Transcript, null=False, on_delete=models.CASCADE)
-    gene = models.CharField(max_length= 15, primary_key=True)
-    gene_biotype =  models.CharField(max_length=10)
-    transcript_biotype = models.CharField(max_length= 15)
-    gene_symbol = models.CharField(max_length=10)
-    description = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.gene
 
--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,29 +1,48 @@
-from genomeBact.models import Transcript
+from django.core import management
+from genomeBact.models import Transcript, Genome
 from django.db import transaction
+
 
 # Penser à regarder la disposition management/commands comme alternative à runscript
 
 def get_term(description):
     
-    
     import re
     
     # Est attendu un pattern comme *Chromosome:1:5528445:1* dans tous les headers des FASTAs
     match = re.search(r':(\d+:\d+)', description)
-    _ =  match.group(1).split(":")
-    _ = list(map(int, _))
+    temp =  match.group(1).split(":")
+    temp = list(map(int, temp))
 
     try: 
-        len(_) == 2
-        return _
+        len(temp) == 2
+        return temp
 
     except AssertionError:
         
         print("Oopsie : %s" % description)
         return None
 
+def get_chromosome(description):
 
-def get_set(absolute_path = "/home/sherman/Documents/M2/WEB/data"):
+    import re
+
+    match = re.search(r"chromosome:(\w+)", description)
+    temp = match.group(1)
+
+    try: 
+        len(temp) == 1
+        return temp
+
+    except AssertionError:
+        
+        print("Mauvais parsing du header du génome de  : %s" % description)
+        return None
+
+
+
+
+def get_data(absolute_path = "/home/sherman/Documents/M2/WEB/data"):
 
 
     import os
@@ -38,11 +57,29 @@ def get_set(absolute_path = "/home/sherman/Documents/M2/WEB/data"):
     other_files = [file for file in file_list if "_cds" not in file and "_pep" not in file]
 
 
+
+    genome_dict = {}
+
+    for file in other_files: 
+
+        strain = file.replace(".fa","")
+        sequence = SeqIO.parse(file,"fasta")
+
+        for seq in sequence:
+
+            
+            genome_dict[strain] = {}
+            genome_dict[strain]["chromosome"] = get_chromosome(seq.description)
+            genome_dict[strain]["sequence"] = str(seq.seq)
+            
+
+
+
     seq_dict = {}
 
     for file in cds_files :
         
-        specie = file.replace("_cds.fa", "" )
+        strain = file.replace("_cds.fa", "" )
         sequences = SeqIO.parse(file, "fasta")
         
         for seq in sequences:
@@ -54,13 +91,13 @@ def get_set(absolute_path = "/home/sherman/Documents/M2/WEB/data"):
             seq_dict[seq.name]["start"] = terms[0]
             seq_dict[seq.name]["stop"] = terms[1]
             seq_dict[seq.name]["NT"] = str(seq.seq)
-            seq_dict[seq.name]["specie"] = specie
+            seq_dict[seq.name]["strain"] = strain
             
             
     
     for file in pep_files :
         
-        specie = file.replace("_pep.fa", "" )
+        strain = file.replace("_pep.fa", "" )
         
         
         sequences = SeqIO.parse(file, "fasta")
@@ -68,12 +105,12 @@ def get_set(absolute_path = "/home/sherman/Documents/M2/WEB/data"):
             
             if seq.name not in seq_dict:
                 
-                raise Exception("Peptide without CDS")
+                raise Exception("Peptide without CDS : %s of %s" % (seq.description, strain))
 
             else :
                 
                 
-                if seq_dict[seq.name]["specie"] == specie : 
+                if seq_dict[seq.name]["strain"] == strain : 
                                 
                     seq_dict[seq.name]["AA"] = str(seq.seq)
                     
@@ -81,30 +118,63 @@ def get_set(absolute_path = "/home/sherman/Documents/M2/WEB/data"):
                     
                     raise Exception("Same transcr_ID for different strains")
 
-    return seq_dict
+    
+    full_dict = {}
+    full_dict["genomes"] = genome_dict
+    full_dict["transcripts"] = seq_dict
+
+    return full_dict
+
+	
 
 
 def run():
 
     try:
 
-        a = get_set()
+        full_dict = get_data()
 
     except AssertionError:
 
         print("Erreur du chargement des CDS et des peptides depuis le multiFASTA")
 
 
-    data = []
-    
-    for seq_id in a.keys():
+    if (Transcript.objects.all().exists() | Genome.objects.all().exists()):
 
-        data.append(Transcript(sequence = a[seq_id]["NT"],
-                                specie = a[seq_id]["specie"],
-                                name = seq_id,
-                                start = a[seq_id]["start"],
-                                stop = a[seq_id]["stop"]))
+        management.call_command("flush")
+
+    genomes_data = []
+    genome_dict = full_dict["genomes"]
+
+    for strain in genome_dict.keys():
+
+        genomes_data.append(Genome(sequence = genome_dict[strain]["sequence"],
+                                chromosome = genome_dict[strain]["chromosome"],
+                                strain = strain))
 
     with transaction.atomic():
+        Genome.objects.bulk_create(genomes_data)
 
-        Transcript.objects.bulk_create(data)
+
+
+    transcripts_dict = full_dict["transcripts"]
+
+    for temp_strain in genome_dict.keys():
+
+        current_genome = Genome.objects.get(strain = temp_strain)
+
+        transcripts_of_current_genome = []
+
+        for tsc_name in transcripts_dict.keys():
+
+            if ( transcripts_dict[tsc_name]["strain"] == temp_strain ):
+
+                transcripts_of_current_genome.append(Transcript(name = tsc_name, 
+                                                                genome = current_genome,
+                                                                seq_cds = transcripts_dict[tsc_name]["AA"],
+                                                                seq_nt = transcripts_dict[tsc_name]["NT"],
+                                                                start = transcripts_dict[tsc_name]["start"],
+                                                                stop = transcripts_dict[tsc_name]["stop"]))
+
+        Transcript.objects.bulk_create(transcripts_of_current_genome)
+
