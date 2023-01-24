e98bd370ab977d24d35a57c9c751231cdb81b13a
Sherman-1 <herman.simon.lm@gmail.com>
2023-01-24 18:15:30 +0100

    Up de toutes les modifs de models_forms_view dans cette branche

---
+++ scripts/reset_db.py
@@ -0,0 +1,55 @@
+
+# RESET WHOLE DATABASE AND LOAD DATA FROM SOURCE FILES
+
+def run():
+
+
+    import os
+    import glob
+    from django.core import management
+
+    current_dir = os.getcwd()   
+
+
+    db_path = current_dir + "/db.sqlite3"
+
+    if os.path.isfile(db_path):
+        os.remove(db_path)
+
+
+    migrations_path = current_dir + "genomeBact/migrations/"
+
+    files = glob.glob(migrations_path + "/*")
+
+
+    if files:
+        for file in files:
+            os.remove(file)
+
+
+    print("Creation des tables ...   \n\n\n")
+
+    try:
+        management.call_command("makemigrations", "genomeBact")
+        management.call_command("migrate")
+
+        print("Creation réussie")
+
+    except Exception:
+
+        print("Echec de création des tables")
+
+
+    print("Chargement des données initiales ")
+    try:
+
+        management.call_command("runscript","load_data")
+        print("Chargement réussi !")
+    
+    except Exception:
+
+        print("Echec du chargement des données")
+
+
+
+    
