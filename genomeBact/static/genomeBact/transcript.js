8867a413d98d7af5853c29a1e74b35df5917416a
atia.safiya@gmail.com <atia.safiya@gmail.com>
2023-02-01 21:42:14 +0100

    #16- Visulalisation Génome

--- genomeBact/static/genomeBact/transcript.js
+++ genomeBact/static/genomeBact/transcript.js
@@ -1,63 +1,70 @@
-var width = 1200;
-var height = 400;
-s_size = 10000;
-var width_svg = 1200-60;
 
-var fenetre= d3.select("#svg_genomic_view").append("svg")
+var width = 1200;
+var height = 200;
+var s_size = 10000;
+var width_svg = 1200-60;
+var seqlen = length;
+let background = "#FBF5F3"
+let cds_color = "#8DAA9D";
+let over_color = "#45425A";
+
+
+let list = list_cds;
+
+var svg= d3.select("#svg_genomic_view").append("svg")
               .attr("width", width)
               .attr("height", height)
-              .attr("id", "fenetre")
-              .style("background", "lightgray")
-              ;
-var svg = d3.select("#svg_g").append("svg")
-              .attr("width", width_svg)
-              .attr("height", height-90)
               .attr("id", "svg")
-              .style("background", "#C1E1C1")
+              .style("background", background)
               ;
 
 genomic_view(s_size);
 function genomic_view(s_size){
   
-  get_axis(0, s_size+1, width);
-  axis(0, s_size);
-  fleche();
-  ellipse(2000,4000,0);
-
-  var svg_triangle = svg.append("g").attr("id", "triangle");
-  triangle(4000,6000,1, svg_triangle);
-  triangle(500,2000,-1, svg_triangle);
-
   var svg_rect = svg.append("g").attr("id", "rectangle");
-  rectangle(0,100,0, svg_rect);
-  rectangle(2000,4000,0, svg_rect);
-  rectangle(8000,9900,0, svg_rect);
+  rectangle(0,30, svg_rect);
+  rectangle(width-30,width, svg_rect);
 
-}
+  axis(1, s_size);
+  //fleche();
+  //d3.select("[id=fleche]")
+  //.attr("transform",'translate(30,0)');
+  
 
-function changeColor(){
-  d3.selectAll("rect")
-    .style("fill", "pink");
+  var svg_cds = svg.append("g").attr("id", "cds")
+  .style('fill', cds_color);
+
+
+  //let list_cds = {"cds_1" :cds_1, "cds_2" :cds_2};
+  let list_coord_cds = {};
+  for(i in list){    
+    list_coord_cds["cds_"+list[i].id] = cds(list[i], 1, s_size, svg_cds);
+  }
+  d3.select("[id=cds]")
+  .attr("transform",'translate(30,0)')
 }
 
 //----------------------AXIS----------------------------
 
-function axis(start, stop){
+function axis(start, s_size){
+  stop = start+s_size-1;
+  
   var xscale = d3.scaleLinear()
     .domain([start, stop])
     .rangeRound([0, width-60]); // rangeRound makes sure there's no decimals ?
     //.range([0, width-60]);
 
+  let t = (stop-start)/5;
 var x_axis = d3.axisBottom()
         .tickFormat(d3.format("d")) // pas de virgule pour les milliers
-       // .tickValues(d3.range(start,stop+1,((stop-start)/5)))
-        .tickValues([0,2000,4000,6000,10000])
-        //.ticks(5) // nombre de graduation sur l'axe mais must be div by 2, 5 or 10
+        .tickValues(d3.range(start,stop+1,((stop-start)/5)))
+        //.tickValues([start, Math.round((start+t)/1000)*1000, Math.round((start+2*t)/1000)*1000 , Math.round((start+3*t)/1000)*1000, Math.round((start+4*t)/1000)*1000,stop+1])
+        //.ticks(5) // nombre de graduation sur l'axe
         .scale(xscale);
     
   d3.select("#genomic_ax").remove();
-  var svg_grad = fenetre.append("g").attr("id", "genomic_ax")
-            .attr("transform", "translate(30, " + 60  +")")
+  var svg_grad = svg.append("g").attr("id", "genomic_ax")
+            .attr("transform", "translate(30, " + 30  +")")
             .call(x_axis)
 }
 
@@ -104,13 +111,13 @@ function genomic_axis(start, nb_pb, width, svg) {
 
     // Graduations
     let grad = axis_graduation(start, (start+nb_pb-1));
-  
+    let y = 20;
     // Axis
     svg.append("line")
      .attr("x1", 0)
-     .attr("y1", 20)
+     .attr("y1", y)
      .attr("x2", width_svg)
-     .attr("y2", 20)
+     .attr("y2", y)
      .style("stroke", "rgb(0,0,0)")
      .style("stroke-width", 1);
   
@@ -124,16 +131,16 @@ function genomic_axis(start, nb_pb, width, svg) {
       if ( Math.trunc(g_size) > 0 ) {
         svg.append("rect")
            .attr("x", x)
-           .attr("y", 20)
+           .attr("y", y)
            .attr("width", g_size)
            .attr("height", 4);
       }
       else {
         svg.append("line")
          .attr("x1", x)
-         .attr("y1", 20)
+         .attr("y1", y)
          .attr("x2", x)
-         .attr("y2", 20+5)
+         .attr("y2", y+5)
          .style("stroke", "rgb(0,0,0)")
          .style("stroke-width", 1);
       }
@@ -141,7 +148,7 @@ function genomic_axis(start, nb_pb, width, svg) {
       let txt_grad = Math.ceil(grad[i]).toString();
       let units = svg.append("text");
       units.attr("x", x-(txt_grad.length * 3) + g_size/2 )
-           .attr("y", 40)
+           .attr("y", y+20)
            .style("fill", "rgb(0,0,0)")
            .attr("font-size", "13");
       units.text(txt_grad);
@@ -156,193 +163,256 @@ function get_axis(d_start, s_size, width) {
 
 //-------------------FORMES VARIÉES --------------------
 
-function fleche(){
-  d3.select("#fleche").remove();
-  var svg_fleche = svg.append("g").attr("id", "fleche");
 
-  svg_fleche.append("polygon")
-            .attr("points", "76.626,120 76.626,110 224.386,110 224.386,105 229.386,115 224.386,125 224.386,120")
-            .style("fill", "rgb(30, 183, 210)");
-}
-
-function rectangle(start, stop, sens, svg_rect){
-  //d3.select("#rect").remove();
+function rectangle(start, stop, svg_rect){
   let width_r = (stop - start);
-  width_r = width_r* (width_svg/s_size);
-  r_svg = svg_rect.append("rect")
-                        .attr("x", start*(width_svg/s_size))
-                        .attr("y", 50)
+  //width_r = width_r* (width_svg/(s_size-1));
+  let r_svg = svg_rect.append("rect")
+                        .attr("x", start)
+                        .attr("y", 0)
                         .attr("width", width_r)
-                        .attr("height", 25)
-                        .style("fill", 'lightblue')
+                        .attr("height", height)
+                        .style("fill", background)
 }
 
-function ellipse(start, stop, sens){
-  d3.select("#ellipse").remove();
-  var svg_ellipse = svg.append("g").attr("id", "ellipse");
+function cds(cds, seq_start, seq_stop, svg_cds){
+  let startY=120
+  let startX = cds.start *(width_svg/(s_size-1));
+  let stopX = cds.stop *(width_svg/(s_size-1));
+  //let length = stopX - startX
+  let coord = "";
+
+  //pas de CDS < 5 pour le moment
+  //que sens +
+  coord += (startX)+","+(startY+20)+" ";
+  coord += (startX)+","+(startY+10)+" ";
+  coord += (stopX-5)+","+(startY+10)+" ";
+  coord += (stopX-5)+","+(startY+5)+" ";
+  coord += (stopX)+","+(startY+15)+" ";
+  coord += (stopX-5)+","+(startY+25)+" ";
+  coord += (stopX-5)+","+(startY+20);
   
-  let width_e = (stop - start);
-  width_e = width_e* (width_svg/s_size);
+  
+  let g_feat = svg_cds.append("g").attr("id", "cds_"+cds.id);
 
-  svg_ellipse.append("ellipse")
-              .attr("cx", (start*((width_svg)/s_size) + width_e/2))
-              .attr("cy", 200)
-              .attr("rx", width_e/2)
-              .attr("ry", 12.5)
-              .style("fill", "rgb(255, 0, 0)")
-}
+  var name = g_feat.append("text");
+  let y_name = 102;
+  name.attr("x", startX+5)
+      .attr("y", y_name)
+      .attr("class", "name")
+      .attr("id", cds.id)
+      .style("fill", cds_color)
+      .attr("visibility", "hidden")
+      .attr("font-size", "13")
+      .text(cds.id);
 
-function triangle(start, stop, sens, svg_triangle){
-  if(sens == -1){
-    start = s_size -start;
-    stop = s_size - stop;
-  }
+  g_feat.append("polygon")
+      .attr("points", coord)
+      .attr("id", "cds_"+cds.id)
+      .on("click", function() { click(cds, seqlen); })
+      .on("mouseover", function() { over(name, g_feat); })
+      .on("mouseout", function() { out(name, g_feat, cds); });
 
-  let width_t = (stop - start);
-  width_t = width_t* (width_svg/s_size);
-
-  let end = start*((width_svg)/s_size);
-  svg_triangle.append("polygon")
-              .attr("points","" + (end+width_t) + ",270 "+ end +",260 "+ end +",280")
-              .style("fill", "yellow");
-
+  return coord;
 }
 
 //---------------------EVENTS----------------------------
 
 var zoom = 1; //zoom
-var step = 2;
-var position = 0; //décalage du svg
+var decalage = s_size/2;
+var conversion = (width_svg/(s_size-1))
+var l_start = 1;
 
-var actual_start = 0;
-var actual_stop = s_size;
+//------------------------ZOOM-----------------------
 
-function zoom_in(){
-    zoom *= step;
-    actual_stop /=step;
-    axis(actual_start, actual_stop);
- 
-    d3.select("[id=rectangle]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
-  
-    d3.select("[id=fleche]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
+document.getElementById("zoom_small_in").onclick = function(){
+  if(s_size*0.75 < 10){move = 10/(s_size-1);}
+  else{ move = 0.75}
 
-    d3.select("[id=ellipse]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
+  zoom /= move;
+  s_size = Math.floor(s_size*move);
 
-    d3.select("[id=triangle]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
-   
-    d3.select("[id=genomic_axis]")
-      .attr("transform", "translate("+ (position) +",0), scale(+"+ zoom +",1)")
+  change_svg(l_start,zoom,s_size);
       
 }
 
-function zoom_out(){
-  // if(actual_start>0 && actual_stop*step >= s_size+actual_start ){ //si en dézoomant on sort du cadre mais que c'est dû à un décalage du start
-  //   actual_start-=decalage;
-  //   actual_stop-=decalage;
-  // }
-  if((actual_stop*step <= s_size)){ // en dézoomant on ne dépasse pas l'axe initial
-    zoom/=step;
+document.getElementById("zoom_small_out").onclick = function(){
 
-    actual_stop *=step;
-    axis(actual_start, actual_stop);
+  if(Math.floor(s_size*1.5) <= seqlen){ // dézoom possible
+    zoom/= 1.5;
+    s_size = Math.floor(s_size*1.5);
 
-    d3.select("[id=rectangle]")
-      .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)");
-
-    d3.select("[id=fleche]")
-      .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)");
-
-    d3.select("[id=ellipse]")
-      .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)");
-
-    d3.select("[id=triangle]")
-      .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)");
-    
-    d3.select("[id=genomic_axis]")
-      .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)");
+    if(l_start+s_size-1 > seqlen){  //décaler le start   
+      move = l_start - seqlen-s_size+1;
+      l_start= seqlen-s_size+1;
+    }
   }
-  else if(actual_start == 0){
-    actual_stop = s_size;
-    zoom = 1;
-    axis(actual_start, actual_stop);
-
-    d3.select("[id=rectangle]")
-      .attr("transform", "scale(+"+ zoom +",1)");
-
-    d3.select("[id=fleche]")
-      .attr("transform", "scale(+"+ zoom +",1)");
-
-    d3.select("[id=ellipse]")
-      .attr("transform", "scale(+"+ zoom +",1)");
-
-    d3.select("[id=triangle]")
-      .attr("transform", "scale(+"+ zoom +",1)");
-
-      d3.select("[id=genomic_axis]")
-      .attr("transform", "scale(+"+ zoom +",1)");
+  else{ //dézoom trop puissant
+    zoom=1;
+    s_size =seqlen;
+    l_start=1;
   }
+  
+  change_svg(l_start,zoom,s_size);
+  
 }
 
-var move = 0;
-var decalage =100;
+document.getElementById("zoom_in").onclick = function(){
+  move=2;
+  if(s_size/2 < 10){move = s_size/10;}
 
-function left(){
-  if( actual_start-decalage >=0){
+  zoom *= move;
+  s_size = Math.floor(s_size/move);
+
+
+  change_svg(l_start,zoom,s_size);
     
-    move+=decalage*(width_svg/s_size)*zoom;
-    position=move;
+}
 
-    actual_start-=decalage;
-    actual_stop-=decalage;
-    axis(actual_start, actual_stop);
+document.getElementById("zoom_out").onclick = function(){
+  move=2;
 
-    d3.select("[id=rectangle]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
+  if(Math.floor(s_size*move) <= seqlen){ // dézoom possible
+    zoom/= move;
+    s_size = Math.floor(s_size*move);
+    if(l_start+s_size-1 > seqlen){ //décaler le start
+      move = l_start - seqlen-s_size+1;
+      l_start= seqlen-s_size+1;
+    }
+  }
+  else{ //dézoom trop puissant
+    zoom=1;
+    s_size =seqlen;
+    l_start=1;
+  }
 
-    d3.select("[id=fleche]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
 
-    d3.select("[id=ellipse]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
+  change_svg(l_start,zoom,s_size);
 
-    d3.select("[id=triangle]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
+}
+
+//-----------------Neutral ZOOM-----------------
+
+document.getElementById("zoom_neutral").onclick = function(){
+  s_size = 10000;
+  zoom=1;
+
+  change_svg(l_start,zoom,s_size);
+  
+}
+
+//----------------DECALAGE---------------------
+document.getElementById("small_left").onclick = function(){ 
    
-    d3.select("[id=genomic_axis]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
-  }
-}
+  if((l_start-decalage) < 1){ move = l_start-1;}
+  else{ move = decalage}
 
-function right(){
-  if( actual_stop+decalage <= s_size){
-    move-=decalage*(width_svg/s_size)*zoom;
-    position=move;
+  l_start-=move;
 
-    actual_start+=decalage;
-    actual_stop+=decalage;
-    axis(actual_start, actual_stop);
 
-    d3.select("[id=rectangle]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
-
-    d3.select("[id=fleche]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
-
-    d3.select("[id=ellipse]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
-
-    d3.select("[id=triangle]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
-
-    d3.select("[id=genomic_axis]")
-    .attr("transform", "translate("+ position +",0), scale(+"+ zoom +",1)")
-  
-  }
+  change_svg(l_start,zoom,s_size);
   
 }
 
+document.getElementById("small_right").onclick = function() {
+
+  if((l_start+s_size+decalage-1) >= seqlen){ move = seqlen-(l_start+s_size-1);}
+  else{ move = decalage}
+
+  l_start+=move;
+
+
+  change_svg(l_start,zoom,s_size);
+}
+
+document.getElementById("big_left").onclick = function(){
+  
+  if((l_start-2*decalage) < 1){ move = l_start-1;}
+  else{ move = 2*decalage}
+
+  l_start-=move;
+
+
+  change_svg(l_start,zoom,s_size);
+}
+
+document.getElementById("big_right").onclick = function() {
+
+  if((l_start+s_size+2*decalage-1) >= seqlen){ move = seqlen-(l_start+s_size-1);}
+  else{ move = 2*decalage}
+
+  l_start+=move;
+
+  change_svg(l_start,zoom, s_size);
+  
+}
             
+//--------VERIFICATION START ET STOP-----------
+function check_size(l_size) {
+  if ( l_size < 10 )            { l_size = 10; }
+  else if ( l_size > seqlen )   { l_size = seqlen; }
+  return l_size;
+}
+
+function check_start(l_start, start) {
+  if ( l_start <= 0 )           { return(-start+1); } //start=1
+  //else if ( start >= seqlen ) { return(-(start-seqlen - s_size + 1));}
+  return -4;
+}
+
+
+//--------MOUSE CLICK--------------------------
+let g_name = genome_name;
+// Mouse click event
+function click(cds, seq_size) {
+  location.assign("/sp/"+g_name+"/"+cds.id +'/');
+  //location.assign("/results/?id="+cds.name+"&size="+seq_size);
+}
+
+// Mouse over event
+function over(name, f_feat) {
+
+  name.attr("visibility","visible");
+  name.style("fill", over_color);
+  f_feat.style("fill", over_color);
+
+}
+
+// Mouse out event
+function out(name, f_feat, cds) {
+  
+    name.attr("visibility","hidden");
+    name.style("fill", cds_color);
+    f_feat.style("fill", cds_color);
+}
+
+
+//---------------------------------------------
+function change_svg(l_start, zoom, s_size){
+  d3.select("[id=rectangle]").remove();
+  var svg_rect = svg.append("g").attr("id", "rectangle");
+  rectangle(0,30, svg_rect);
+  rectangle(width-30,width, svg_rect);
+
+  axis(l_start, s_size);
+  
+  position=-l_start*(width_svg/(s_size-1));
+
+ d3.select("[id=cds]")
+ .attr("transform", "translate("+ (position+30) +",0), scale(+"+ zoom +",1)")
+}
+
+function check_cds(cds, coord, l_start, s_size){
+  // let seq_stop = l_start+s_size-1;
+  // //start out
+  // if(cds.stop < l_start){
+    
+  // }
+  // //stop out
+  // if(cds.start > seq_stop){
+
+  // }
+  d3.select("[id=cds_first]")
+      .style("fill", "green")
+      .attr("points", coord);
+}
