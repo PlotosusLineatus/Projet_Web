977330c05b87cf3c87d6e00ae636a1e73e92b377
Sherman-1 <herman.simon.lm@gmail.com>
2023-02-10 20:14:22 +0100

    add input data to folder and change path

--- scripts/utils.py
+++ scripts/utils.py
@@ -145,7 +145,7 @@ def get_chromosome(description):
 
 
 
-def get_data(absolute_path = "/Users/Soeur/Documents/ECOLE/ORSAY/M2/9.web/PROJET/data_less"):
+def get_data(absolute_path = "./data"):
 #def get_data(absolute_path = "/home/noemie/Documents/data"):
 #def get_data(absolute_path = "/home/sherman/Documents/M2/WEB/data"):
 
