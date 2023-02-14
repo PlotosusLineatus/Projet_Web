42153e9b6f9c78838bf5c6e536940e654c09de1a
atia.safiya@gmail.com <atia.safiya@gmail.com>
2023-02-14 16:36:35 +0100

    pb commit (Modify)

--- genomeBact/views/users.py
+++ genomeBact/views/users.py
@@ -204,7 +204,8 @@ def workspace(request):
     transcripts_to_annotate = request.user.profile.to_annotate.all()
     transcripts_to_assign = Transcript.objects.filter(status = 'empty')
     transcripts_to_validate = Transcript.objects.filter(status = 'annotated', validator = request.user.username)
-    annotators = User.objects.filter(groups__name='Annotator') | User.objects.filter(groups__name='Admin')
+    #annotators = User.objects.filter(groups__name='Annotator') | User.objects.filter(groups__name='Admin')
+    annotators = User.objects.filter().exclude(groups__name='Reader')
 
     nb_to_assign = Transcript.objects.filter(status = 'empty').count()
     nb_to_val =  Transcript.objects.filter(status = 'annotated', validator = request.user.username).count()
